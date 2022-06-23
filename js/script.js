e3de8dadb683c4c47622eabab8979098d74dcad1
Roma <lianviks1@gmail.com>
2022-06-23 18:12:14 +0600

    personalMovie

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 "use strict";
-//const result = confirm ("are you here?");
-//console.log (result);
-const answer = prompt('est 18?',"18");
-console.log(typeof(answer));
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?","");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?',''),
+      b = prompt('на сколько оцените его'),
+      c = prompt('Один из последних просмотренных фильмов?',''),
+      d = prompt('на сколько оцените его');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
