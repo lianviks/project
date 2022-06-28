96ac5f3e5f6445064c6e794bf33cac37a904a412
Roma <lianviks1@gmail.com>
2022-06-28 13:24:46 +0600

    triangle

--- js/script.js
+++ js/script.js
@@ -1,20 +1,5 @@
-"use strict";
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?","");
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-const a = prompt('Один из последних просмотренных фильмов?',''),
-      b = prompt('на сколько оцените его'),
-      c = prompt('Один из последних просмотренных фильмов?',''),
-      d = prompt('на сколько оцените его');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
+let i = 0;
+while (i < 16){
+    console.log(i);
+    i++;
+}
